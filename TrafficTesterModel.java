95c2d5f87ed2cc3ea4a9e0e87628535f23066584
Alex-Ginsberg <alex.ginsberg@richmond.edu>
2016-11-09 10:51:22 -0500

    Added corrections for "Stage 0"

--- TrafficTesterModel.java
+++ TrafficTesterModel.java
@@ -1,6 +1,12 @@
 // Programmers: GOATS team: Alex David Rex Tom Zihao
-// TrafficTesterModel.java
-// Latest Enhancement: Corrected comments
+// *****************************************************************************
+// *****************************************************************************
+// **** TrafficTesterModel
+// *****************************************************************************
+// *****************************************************************************
+// The purpose of the TrafficTesterModel is to create a grid given a provided
+// numRows and numCols.  Provided cars are then inserted into the grid.
+// Latest Enhancement: Stage 0 Corrections
 //////////////// 80 characters /////////////////////////////////////////////////
 import java.util.ArrayList;
 
@@ -11,7 +17,6 @@ public class TrafficTesterModel{
 	//************Constructor************
 	public TrafficTesterModel(int numRows, int numCols, 
 	ArrayList< ArrayList<Integer> > carParameters){
-
 		//System.out.println("In TrafficTesterModel(): ");
         //********Creating a new grid********
         //System.out.println("  Constructing a Grid...");
@@ -22,10 +27,15 @@ public class TrafficTesterModel{
         g.insertCars(carParameters);
 
         //System.out.println("Exiting TrafficTesterModel()");
-	}
+	}// End of TrafficTesterModel constructor 
+
+
 
     public void run(){
         //System.out.println("In TrafficTesterModel: running simulation");
         g.update();
-    }
+    }// End of run method
+
+
+
 }
