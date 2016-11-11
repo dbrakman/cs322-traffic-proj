78ca89199d6ebb9fca9d444545bbaffe719368ad
David Brakman <david.brakman@gmail.com>
2016-11-11 12:47:23 -0500

    Replaced each TAB with 4 spaces

--- Car.java
+++ Car.java
@@ -10,46 +10,46 @@
 // instance of Car, and provides such information if needed.
 
 public class Car{
-	//**************Instance Variables**************
-	int numBlocksBeforeTurning;
-	int turnDirectionCode;
-	int carID;
+    //**************Instance Variables**************
+    int numBlocksBeforeTurning;
+    int turnDirectionCode;
+    int carID;
 
-	//**************Constructor**************
-	public Car(int initialNumBlocks, int turnDirectionCode, int carID){
-		this.numBlocksBeforeTurning = initialNumBlocks;
-		this.turnDirectionCode = turnDirectionCode;
-		this.carID = carID;
-	}//End of car constructor
-
-
-
-	//**************Instance Methods**************
-	//After a car moves from one lane to another, update() is called to 
-	//decrement the numBlocksBeforeTurning 
-	public void update(){
-		this.numBlocksBeforeTurning--;
-	}//End of update() method
+    //**************Constructor**************
+    public Car(int initialNumBlocks, int turnDirectionCode, int carID){
+        this.numBlocksBeforeTurning = initialNumBlocks;
+        this.turnDirectionCode = turnDirectionCode;
+        this.carID = carID;
+    }//End of car constructor
 
 
 
-	//Determines which direction a car will be moving.
-	//If its numBlocksBeforeTurning is 0, the it will return the car's 
-	//turnDirectinoCode.
-	//Otherwise, it will return -1, which indicates it is moving straight
-	public int getCurrentDirection(){
-		if (this.numBlocksBeforeTurning == 0){
-			return this.turnDirectionCode;
-		}//end of if(this.numBlocksBeforeTurning == 0)
-		else{
-			return 0;
-		}
-	}//End of getCurrentDirection() method
+    //**************Instance Methods**************
+    //After a car moves from one lane to another, update() is called to 
+    //decrement the numBlocksBeforeTurning 
+    public void update(){
+        this.numBlocksBeforeTurning--;
+    }//End of update() method
 
 
 
-	//Return the ID of a car
-	public int getID(){
-		return this.carID;
-	}//End of getID() method
+    //Determines which direction a car will be moving.
+    //If its numBlocksBeforeTurning is 0, the it will return the car's 
+    //turnDirectinoCode.
+    //Otherwise, it will return -1, which indicates it is moving straight
+    public int getCurrentDirection(){
+        if (this.numBlocksBeforeTurning == 0){
+            return this.turnDirectionCode;
+        }//end of if(this.numBlocksBeforeTurning == 0)
+        else{
+            return 0;
+        }
+    }//End of getCurrentDirection() method
+
+
+
+    //Return the ID of a car
+    public int getID(){
+        return this.carID;
+    }//End of getID() method
 }
--- TrafficTesterModel.java
+++ TrafficTesterModel.java
@@ -11,27 +11,27 @@
 import java.util.ArrayList;
 
 public class TrafficTesterModel{
-	Grid g;
-	//int t;
+    Grid g;
+    //int t;
 
-	//************Constructor************
-	public TrafficTesterModel(int numRows, int numCols, 
-	ArrayList< ArrayList<Integer> > carParameters){
-		//System.out.println("In TrafficTesterModel(): ");
+    //************Constructor************
+    public TrafficTesterModel(int numRows, int numCols, 
+    ArrayList< ArrayList<Integer> > carParameters){
+        //System.out.println("In TrafficTesterModel(): ");
         //********Creating a new grid********
         //System.out.println("  Constructing a Grid...");
-		g = new Grid(numRows, numCols);
+        g = new Grid(numRows, numCols);
 
-		//********Teling the grid to make and insert cars********
-		//System.out.println("  Telling the grid to make and insert cars...");
+        //********Teling the grid to make and insert cars********
+        //System.out.println("  Telling the grid to make and insert cars...");
         g.insertCars(carParameters);
 
         //System.out.println("Exiting TrafficTesterModel()");
-	}// End of TrafficTesterModel constructor 
+    }// End of TrafficTesterModel constructor 
 
 
 
-	//Calls update() on the Grid, causing the intersections to be looked at
+    //Calls update() on the Grid, causing the intersections to be looked at
     public void run(){
         //System.out.println("In TrafficTesterModel: running simulation");
         g.update();
