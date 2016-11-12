ecd0bbffec9ca166d6d034c67dfd84b32a2cf494
David Brakman <david.brakman@gmail.com>
2016-11-12 00:30:24 -0500

    Added capacity and time to traverse

--- Lane.java
+++ Lane.java
@@ -5,10 +5,11 @@
 // **** Lane 
 // *****************************************************************************
 // *****************************************************************************
-// Latest Enhancement: Corrected style rules for stage_0
+// Latest Enhancement: Added capacity and minimum time to traverse, for stage_1
 
 import java.util.Queue;
-import java.util.LinkedList;
+import java.util.LinkedList; //The implementation of a queue
+import java.util.HashMap;
 
 //Lane Class
 //This class defines a "Lane" object, which will hold cars.
@@ -19,44 +20,87 @@ import java.util.LinkedList;
 
 public class Lane {
     //**************Instance Variables**************
-    public Queue<Car> q;
-    public boolean boundary;
+    private Queue<Car> q; // Stores the cars in the lane
+    private HashMap<Car,int> timesInQueue;  // Stores the length of time each
+                                            // car spends in the lane
+    private int capacity;
+    private int minTimeToTraverse;
 
     //**************Constructor**************
-    public Lane() {
+    public Lane(int capacity, int minTimeToTraverse)
+    {
         q = new LinkedList<Car>();
-        boundary = false;
-    }//end of constructor
+        timesInQueue = new HashMap<Car,int>();
+        this.capacity = capacity;
+        this.minTimeToTraverse = minTimeToTraverse;
+    } // end of constructor
 
 
 
     //**************Instance Methods**************
     //removes a Car from a Lane and returns it
-    public Car get() {
-        return q.poll();
-    }//end of get
+    public Car get()
+    {
+        // Since only one car can exit a lane during a given timestep
+        // We can "update" the length of time each car spends in the lane
+        // in this get() method:
+        for( Car c : q )
+        {   // Increment c's timeInQueue
+            timesInQueue.put(c,timesInQueue.get(c)+1);
+        }
+        if( this.peek() != null)
+        {
+            // if a car is able to exit, remove it from the queue
+            return q.poll();
+        } else {
+            // return null if no car is able to exit the queue yet.
+            return null;
+        }
+    } // end of get()
+
+
+
+    public Car peek()
+    {
+        Car c = q.peek();
+        if(timesInQueue.get(c) >= minTimeToTraverse)
+        {
+            return c;
+        } else {
+            // return null if no car is able to exit the queue yet.
+            return null;
+        }
+    } // end of peek()
+
 
 
 
     //adds a provided Car c to the Lane
-    public boolean add(Car c) {
-        if (this.boundary == true){
-            System.out.println("   car#" + c.getID() + " leaves the grid");
-        }//end of if (this.boundary == true)
+    public boolean add(Car c)
+    {
+        if(this.isFull())
+        {
+            System.out.println("Illegal attempt to add to a full lane");
+            return;
+        } // end of if(q.size() >= capacity)
+        timesInQueue.put(c,0);
         return q.add(c);
-    }//end of add
+    } // end of add(Car c)
 
 
 
-    public boolean isEmpty() {
+    public boolean isEmpty()
+    {
         return (q.peek() == null);
-    }//end of isEmpty
+    } // end of isEmpty()
 
+    
 
+    public boolean isFull()
+    {
+        return q.size() >= capacity;
+    } // end of isFull()
 
-    public void setIsBoundary() {
-        this.boundary = true;
-    }//end of setIsBoundary
 
 
 }
