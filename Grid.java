4ba61f5ede0eeb29582da85f2ac4bae355fcfd51
Alex-Ginsberg <alex.ginsberg@richmond.edu>
2016-11-14 22:12:20 -0500

    Added setBoundaryLanes() and called it in the constructor

--- Grid.java
+++ Grid.java
@@ -72,25 +72,8 @@ public class Grid
         } // end of for(int rowNum = 0; rowNum < numRows; rowNum++)
         // The above code constructs lanes and intersections, populates grid */
 
-        //Creation of fire intersections
-        for (int colNum = 1; colNum <= numCols; colNum++){
-            //Insert a fire intersection at i[0][colNum], only needs 1 inLane
-            //corresponding to the southward outlane of i[1][colNum]
-            
-            //Insert a fire intersection at i[numRows + 1][colNum], only needs 
-            //inLane corresponding to the northward outLane of 
-            //i[numRows][colNum]
-        } //end of for (int colNum = 0; colNum <= numCols + 1; colNum++)
-        for (int rowNum = 1; rowNum <= numRows; rowNum++){
-            //Insert a fire intersection at i[rowNum][0], only needs 1 inLane
-            //corresponding to the wetsward outLane of i[rowNum][1]
-            
-            //Insert a fire intersection at i[rowNum][numCols + 1], only needs
-            //1 inLane corresponding to the eastward outLane of 
-            //i[rowNum][numCols]
-        } //end of (for int rowNum = 0; rowNum <= numRows + 1; rowNum++)
-
-
+        this.setBoundaryLanes();
+        
     } // end of Grid(int numRows, int numCols) 
 
     
@@ -241,9 +224,32 @@ public class Grid
         i[rowNum][colNum] = new Intersection(inLanes,outLanes);
     } // end of shareNoLanes(int rowNum, int colNum)
 
+    private void setBoundaryLanes(){
+        //Set boundary lanes for the bottom row
+        for (int colNum = 1; i <= numCols; colNum++){
+            (i[1][colNum].getOutLane(0)).setIsBoundary();
+        } //end for (int colNum = 1; i <= numCols; colNum++)
+
+        //Set boundary lanes for the top row
+        for (int colNum = 1; i <= numCols; colNum++){
+            (i[numRows][colNum].getOutLane(2)).setIsBoundary();
+        } //end for (int colNum = 1; i <= numCols; colNum++)
+
+        //Set boundary lanes for the west side
+        for (int rowNum = 1; i <= numRows; rowNum++){
+            (i[rowNum][1].getOutLane(3)).setIsBoundary();
+        } // end for (int rowNum = 1; i <= numRows; rowNum++)
+
+        //Set boundary lanes for the east side
+        for (int rowNum = 1; i <= numRows; rowNum++){
+            (i[rowNum][numCols].getOutLane(2)).setIsBoundary();
+        } // end for (int rowNum = 1; i <= numRows; rowNum++)
+
+    }
+    }
+
 
 
 
 
 }
-
